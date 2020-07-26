8561c6d6c6adf59eaf42c08ff1528ad6bf89adb4
isaboll1 <isaboll12@gmail.com>
2020-07-26 02:23:21 -0400

    for the sake of the CI.

--- VkBuildDebug.sh
+++ VkBuildDebug.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-g++ -DVK_DEBUG -Wall -Wextra src/*.cpp -o bin/VKEngineDEBUG.x86_64 -lSDL2 -lvulkan
+g++ -std=c++17 -DVK_DEBUG -Wall -Wextra src/*.cpp -o bin/VKEngineDEBUG.x86_64 -lSDL2 -lvulkan
--- VkBuildRelease.sh
+++ VkBuildRelease.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-g++ -Wall -Wextra src/*.cpp -o bin/VKEngine.x86_64 -lSDL2 -lvulkan
+g++ -std=c++17 -Wall -Wextra src/*.cpp -o bin/VKEngine.x86_64 -lSDL2 -lvulkan
